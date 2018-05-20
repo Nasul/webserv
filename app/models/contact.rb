e562472283fa5ecc3d2a32e2fdf5816911fb2775
Kyle <tj.khujand@gmail.com>
2018-05-20 18:46:32 +0000

    Added form validation to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base 
-
+  validates :company, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
